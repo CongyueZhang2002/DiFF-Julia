3b6920e9545479897ab1439bba78ff079b406e28
CongyueZhang2002 <maxzhang2002@g.ucla.edu>
2026-04-30 00:00:28 -0700

    Update SIDIS EEC workflow

--- Processes/SIDIS/SIDIS_EEC.jl
+++ Processes/SIDIS/SIDIS_EEC.jl
@@ -11,7 +11,7 @@ function SIDIS_EEC_y(; x::Real, Q::Real)
     return y
 end
 
-function SIDIS_EEC(; chi::Real, x::Real, Q::Real, μ::Real)
+function SIDIS_EEC_unnormalized(; chi::Real, x::Real, Q::Real, μ::Real)
 
     μ = SIDIS_EEC_safe_μ(μ)
     y = SIDIS_EEC_y(x=x, Q=Q)
@@ -25,49 +25,55 @@ function SIDIS_EEC(; chi::Real, x::Real, Q::Real, μ::Real)
     return sin(chi) / 2 * prefactor * structure
 end
 
-function SIDIS_EEC_batch(; chi_array::AbstractVector, x::Real, Q::Real, μ::Real)
+function SIDIS_EEC(; chi::Real, Q::Real, μ::Real, x=nothing)
 
     μ = SIDIS_EEC_safe_μ(μ)
-    y = SIDIS_EEC_y(x=x, Q=Q)
-    U = D1_EEC_own_precompute_evolution(mb, μ)[1, 1]
+    z = (1 - cos(chi)) / 2
 
-    prefactor = 2*pi*alpha_qed(Q)^2/Q^2
-    structure = (1+(1-y)^2)/y * sum(eq2_vec(μ) .* get_f1(x, μ)) * U
+    Dq, _ = D1_EEC_own(z, Q, μ)
+
+    return sin(chi) / 2 * Dq
+end
+
+function SIDIS_EEC_batch(; chi_array::AbstractVector, Q::Real, μ::Real, x=nothing)
+
+    μ = SIDIS_EEC_safe_μ(μ)
+    U = D1_EEC_own_precompute_evolution(mb, μ)[1, 1]
 
     predictions = Vector{Float64}(undef, length(chi_array))
     @inbounds for i in eachindex(chi_array)
         chi = chi_array[i]
         z = (1 - cos(chi)) / 2
-        predictions[i] = sin(chi)/ 2 * D1_EEC_own_initial_quark(z, Q) * prefactor * structure
+        predictions[i] = sin(chi)/ 2 * D1_EEC_own_initial_quark(z, Q) * U
     end
 
     return predictions
 end
 
-function SIDIS_EEC_pmap(; chi_array::AbstractVector, x_array::AbstractVector, Q_array::AbstractVector, μ_array::AbstractVector)
+function SIDIS_EEC_pmap(; chi_array::AbstractVector, Q_array::AbstractVector, μ_array::AbstractVector, x_array=nothing)
     
     n = length(chi_array)
-    length(x_array) == n || throw(ArgumentError("x_array must have the same length as chi_array"))
     length(Q_array) == n || throw(ArgumentError("Q_array must have the same length as chi_array"))
     length(μ_array) == n || throw(ArgumentError("μ_array must have the same length as chi_array"))
+    if x_array !== nothing
+        length(x_array) == n || throw(ArgumentError("x_array must have the same length as chi_array"))
+    end
 
-    args_vec = collect(zip(chi_array, x_array, Q_array, μ_array))
+    args_vec = collect(zip(chi_array, Q_array, μ_array))
 
     predictions = nothing
     t = @elapsed begin
         if n > 0 &&
-           all(==(x_array[1]), x_array) &&
            all(==(Q_array[1]), Q_array) &&
            all(==(μ_array[1]), μ_array)
             predictions = SIDIS_EEC_batch(
                 chi_array = chi_array,
-                x = x_array[1],
                 Q = Q_array[1],
                 μ = μ_array[1],
             )
         else
-            predictions = pmap(args_vec; batch_size = 1) do (chi, x, Q, μ)
-                SIDIS_EEC(chi = chi, x = x, Q = Q, μ = μ)
+            predictions = pmap(args_vec; batch_size = 1) do (chi, Q, μ)
+                SIDIS_EEC(chi = chi, Q = Q, μ = μ)
             end
         end
     end
